28681e2db3197aa874051208ed750e7901378023
slfiene <sean.fiene@nintex.com>
2017-07-05 08:49:55 -0700

    startTimer();

--- switch-magnetic-door.js
+++ switch-magnetic-door.js
@@ -5,8 +5,9 @@ var board = new five.Board({
 });
 var status;
 
-function startTimer() {
-    console.log("starting this up!");
+var startTimer = function () {
+    console.log("Timer Started!");
+    setTimeout(startTimer, 10000);
 }
 
 board.on("ready", function () {
@@ -16,16 +17,12 @@ board.on("ready", function () {
     sw.on("open", function () {
         console.log("open");
         let status = false;
-
     });
 
     sw.on("close", function () {
         console.log("close");
         let status = true;
-
-        if (status = true) {
-            clearTimeout();
-        }
-
+        startTimer();
     });
+
 });
